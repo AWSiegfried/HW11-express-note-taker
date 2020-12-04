bc99e92fe0698026de67ed5d5ced7606aa3ef7b0
AWSiegfried <awsiegfried@gmail.com>
2020-12-03 17:11:58 -0800

    updating layout

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 //Dependencies
-//We need to require a llof our depenacies
 const express = require("express");
-const { fstat } = require("fs");
+const fs = require("fs");
 const app = express();
+const db = require("./Develop/db/db.json")
+
 
 //GLobal Variables
-//Declaring global variables
 const PORT = process.env.PORT || 10000;
-const db = require("./db/db.json")
-console.log(db);
 let num = 0;
 
 //Middlewear
@@ -17,21 +15,21 @@ app.use(express.urlencoded({ extended: true }));
 //Turn data into JSON format
 app.use(express.json());
 
-//We need a middlewear to have our css and javascript files to be loaded
-app.use(express.static("public"));
+//We need a middlewear to have our css and javascript files to be loaded.  Essentially resets the file path starting at public
+app.use(express.static("./Develop/public"));
 
 //Routes
 //HTML ROUTES HERE
 //We're gonna need to create a route to serve our html files so when we hit those endpoints, the browser will serve our html to us. 
-//Our goal is to display what's in teh index.html to the root route I.E localhost:10000
+//Our goal is to display what's in the index.html to the root route I.E localhost:10000
 //fs module, path
 //We are defining our home routes, which takes in a callback function with request and response
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "/index"))
 })
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile(path.join(__dirname, "/notes"))
 })
 
 app.get("/api/test", (req, res) => {
@@ -50,17 +48,13 @@ app.get("/api/test", (req, res) => {
 //What are our tools.   Express get, express post, express delete
 //2 way s of hanlding this proble. 1. make a variable to store teh db.json file.  2. we need to use fsreadfile if we want to grab the data and send it to the front-end
 app.get("/api/notes", (req, res) => {
-        res.json(db);
-    })
-    //Whend doing a post request remember to console.log the req.body
-    //A post follows similar syntax using req and res as the callback
-    //becasue we declare our db at the very top 
+    res.json(db);
+});
+//Whend doing a post request remember to console.log the req.body
+//A post follows similar syntax using req and res as the callback
+//becasue we declare our db at the very top 
 
 //SERVER LISTENER
 app.listen(PORT, () => {
     console.log("You started up the server")
 })
-
-
-
-//Express, kill-port, nodemon
